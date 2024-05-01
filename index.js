07393d4ee5f02db032ebabdcc57c435885bd533d
Steward Lumowa <stewardyohanes29@gmail.com>
2024-05-01 15:31:27 +0800

    array of object

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 const path = require("path");
 
 // setting variables, configurations, etc.
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./views"));
 
+// Middleware
+app.use(express.static(path.join(__dirname, "./assets")));
+
 // body parser
 app.use(express.urlencoded({ extended: false }));
-
-// Middleware
-app.use(express.static(path.join(__dirname, "/assets")));
 
 // Routes
 app.get("/", home);
 app.get("/project", project);
+app.post("/project", addProject);
 app.get("/add-project", addProjectView);
-app.post("/add-project", addProject);
+app.get("/project/:id", detailProject);
 
 const data = [];
 
@@ -27,7 +28,7 @@ function home(req, res) {
 }
 
 function project(req, res) {
-  res.render("project");
+  res.render("project", { data });
 }
 
 function addProjectView(req, res) {
@@ -35,7 +36,44 @@ function addProjectView(req, res) {
 }
 
 function addProject(req, res) {
-  console.log(req.body);
+  const { projectName, startDate, endDate, description, technologies } =
+    req.body;
+
+  const timeDiff = Date.parse(endDate) - Date.parse(startDate);
+  const diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
+  const diffMonths = Math.floor(diffDays / 30.4167);
+  const diffYears = Math.floor(diffMonths / 12);
+
+  let duration;
+
+  if (diffYears > 0) {
+    duration = `${diffYears} Years`;
+  } else if (diffMonths > 0) {
+    duration = `${diffMonths} Months`;
+  } else {
+    duration = `${diffDays} Days`;
+  }
+
+  data.unshift({
+    id: data.length + 1,
+    projectName,
+    startDate,
+    endDate,
+    description,
+    technologies,
+    duration,
+    file: "https://plus.unsplash.com/premium_photo-1688045530445-66a06a5e9ba6?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  });
+  // console.log(data);
+  res.redirect("/project");
+}
+
+function detailProject(req, res) {
+  const { id } = req.params;
+  console.log(id);
+  const selectedProject = data.filter((project) => project.id == id);
+
+  res.render("detail-project", { selectedProject });
 }
 
 app.listen(port, () => {
